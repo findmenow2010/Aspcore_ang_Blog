732a2c1318af818bac110465aedb54726cc3f025
Vehicle Soft <vehiclesoft@gmail.com>
2020-02-19 13:48:54 +0200

    intial commit

---
+++ Blog/Data/BlogPostsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Blog.Models;
+
+    public class BlogPostsContext : DbContext
+    {
+        public BlogPostsContext (DbContextOptions<BlogPostsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog.Models.BlogPost> BlogPost { get; set; }
+    }
